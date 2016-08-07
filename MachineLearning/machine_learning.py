f92c7cdbe9bda37c81e38f132b0a90070d55d2df
EltonBroering <eltonbroering_14@hotmail.com>
2016-08-07 16:24:51 -0300

    Add approximation function

--- MachineLearning/machine_learning.py
+++ MachineLearning/machine_learning.py
@@ -19,6 +19,11 @@ roubos = data[:,1]
 anos = anos[~sp.isnan(anos)]
 roubos = roubos[~sp.isnan(roubos)]
 
+fp1 = sp.polyfit(anos, roubos, 4)
+f1 = sp.poly1d(fp1)
+
+
+plt.plot(anos, f1(anos), linewidth=2)
 plt.scatter(anos,roubos)
 plt.title("Proporcao de roubos de carros por ano em Porto Alegre")
 plt.xlabel("Ano")
@@ -26,3 +31,4 @@ plt.ylabel("Numero de roubos")
 plt.autoscale(tight=True)
 plt.grid()
 plt.show()
+
